252354f57378bdcb0c2f13486a0c3d3165aa36d9
Alyssa Simon <alysimon519@gmail.com>
2024-03-28 16:16:43 -0400

    change fadein value

--- script.js
+++ script.js
@@ -4,5 +4,5 @@
 const profile = document.querySelector(".profile");
 const arrow = document.querySelector(".arrow");
 $(document).ready(function(){
-    $(profile).fadeIn("8000");
+    $(profile).fadeIn("12000");
 });
