c8b6e694b479794c9a263a8a4577669ec4608605
Alyssa Simon <alysimon519@gmail.com>
2024-03-28 16:11:14 -0400

    add fadein for portfolio using jquery

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 /* TODO: 
 - use jQuery */
+
+const profile = document.querySelector(".profile");
+const arrow = document.querySelector(".arrow");
+$(document).ready(function(){
+    $(profile).fadeIn("8000");
+});
